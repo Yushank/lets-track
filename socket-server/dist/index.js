7e3b8157e5342c0f49a75136384da89c6b418d0e
Yushank <yushank2001@gmail.com>
2025-07-06 20:28:59 +0530

    npx tsc in socket-server to fix npm build issue in railway

---
+++ socket-server/dist/index.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*", //allow all for now
+        methods: ["GET", "POST"]
+    }
+});
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST"]
+}));
+app.use(express.json());
+//socket connection
+io.on('connection', (socket) => {
+    console.log("user connected", socket.id);
+    socket.on('disconnect', () => {
+        console.log("user disconnected:", socket.id);
+    });
+    // Error handling
+    socket.on("error", (err) => {
+        console.error("Socket error:", err);
+    });
+});
+//HTTP route to emit
+app.post('/emit-meal', (req, res) => {
+    console.time("emit-process");
+    const meal = req.body;
+    if (!meal) {
+        return res.status(400).json({ error: "No meal data provided" });
+    }
+    console.log("Emitting meal:", meal);
+    io.emit('newMeal', meal);
+    res.json({ status: 'Meal emitted' });
+    console.timeEnd("emit-process");
+});
+//start server
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+    console.log(`Socket server is running on ${PORT}`);
+});
