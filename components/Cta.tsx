3ec2a4ded543a4c7e1442f046cd6c84cbec3da9a
Yushank <yushank2001@gmail.com>
2025-11-10 19:16:50 +0530

    cta section banner text animation

--- components/Cta.tsx
+++ components/Cta.tsx
@@ -1,13 +1,75 @@
-import React from "react";
+"use client";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+import React, { useEffect } from "react";
 
 export const Cta = () => {
+  useEffect(() => {
+    gsap.registerPlugin(SplitText, ScrollTrigger);
+
+    document.fonts.ready.then(() => {
+      const split = new SplitText(".SplitText", {
+        type: "words,lines",
+        linesClass: "line",
+      });
+
+      // gsap.set(split.lines, { yPercent: 100, opacity: 0 });
+
+      // gsap.set(".Split", { opacity: 1 });
+
+      gsap.fromTo(
+        split.lines,
+        {
+          yPercent: 100,
+          opacity: 0,
+        },
+        {
+          duration: 0.6,
+          yPercent: 0,
+          opacity: 1,
+          stagger: 0.1,
+          ease: "expo.out",
+          scrollTrigger: {
+            trigger: ".SplitText",
+            start: "top 80%",
+            toggleActions: "play none none none",
+            markers: true,
+            scrub: 1,
+          },
+        }
+      );
+
+      gsap.fromTo(
+        ".FadeText",
+        {
+          opacity: 0,
+        },
+        {
+          duration: 2,
+          opacity: 1,
+          scrollTrigger: {
+            trigger: ".FadeText",
+            start: "top 80%",
+            scrub: 1,
+          },
+        }
+      );
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
+
   return (
     <section className="min-h-screen text-center">
       <div className="bg-green-500 py-10 text-center">
-        <h1 className="text-5xl font-semibold text-gray-50 pt-2">
+        <h1 className="SplitText text-5xl font-semibold text-gray-50 pt-2">
           No more "food not found" moments
         </h1>
-        <p className="py-4 text-gray-700">
+        <p className="FadeText py-4 text-gray-700">
           No matter what’s on your plate — our AI calculates it.
         </p>
       </div>
